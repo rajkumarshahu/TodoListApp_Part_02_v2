4aaddf9733ddf252238b3e4ebb7ef99966f94eb1
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2020-11-26 15:11:56 -0500

    Initial Commit

---
+++ TodoListApp_Part_02_v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoListApp_Part_02_v2
+//
+//  Created by Raj Kumar Shahu on 2020-11-26.
+//  Copyright © 2020 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
